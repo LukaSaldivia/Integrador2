8415acd8ee435817e901157eab509136f5cb5053
Luka Saldivia <saldivialuka@gmail.com>
2025-10-06 18:48:47 -0300

    refactor ReporteDTO

--- src/main/java/ar/unicen/exa/aldesal/dto/ReporteDTO.java
+++ src/main/java/ar/unicen/exa/aldesal/dto/ReporteDTO.java
@@ -8,18 +8,12 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class ReporteDTO {
     private Integer id;
     private String nombre;
-    private int inscriptos;
-    private int graduados;
+    private Long inscriptos;
+    private Long graduados;
     private int anio;
 
-    public ReporteDTO(Integer id, String nombre, int inscriptos, int graduados, int anio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.inscriptos = inscriptos;
-        this.graduados = graduados;
-        this.anio = anio;
-    }
 }
