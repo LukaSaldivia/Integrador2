346b488d6adbae050ff7ff272ccaeb6839fcdbe7
Luka Saldivia <saldivialuka@gmail.com>
2025-10-06 18:24:32 -0300

    fix CarreraRepositoryImpl.getReporte

--- src/main/java/ar/unicen/exa/aldesal/repository/impl/CarreraRepositoryImpl.java
+++ src/main/java/ar/unicen/exa/aldesal/repository/impl/CarreraRepositoryImpl.java
@@ -67,6 +67,7 @@ public class CarreraRepositoryImpl implements CarreraRepository {
     public EstadoOperacionDTO<List<ReporteDTO>> getReporte() {
 
         List<ReporteDTO> response = null;
+        em.getTransaction().begin();
 
         try {
             String query = """
@@ -83,9 +84,13 @@ public class CarreraRepositoryImpl implements CarreraRepository {
 
             response = em.createQuery(query, ReporteDTO.class).getResultList();
         }catch (Exception e) {
+            em.getTransaction().commit();
+            em.close();
             return new EstadoOperacionDTO<>(false, null);
         }
 
+        em.getTransaction().commit();
+        em.close();
         return new EstadoOperacionDTO<>(true, response);
     }
 }
